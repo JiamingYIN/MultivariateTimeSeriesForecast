39b2002894dc4d0b3af00cd3512db8395cca7aa2
Jiaming YIN <874694453@qq.com>
2019-06-23 08:57:11 +0800

    Update metric.py

--- utils/metric.py
+++ utils/metric.py
@@ -1,1 +1,25 @@
+import numpy as np
 
+
+def normal_std(x):
+    return x.std() * np.sqrt((len(x) - 1.)/(len(x)))
+
+
+def RMSE(v, v_):
+    return np.sqrt(np.mean((v_ - v) ** 2))
+
+
+def MAE(v, v_):
+    return np.mean(np.abs(v_ - v))
+
+
+def MAPE(v, v_):
+    return np.mean(np.abs(v_ - v) / v)
+
+
+def get_acc():
+    pass
+
+
+def RSE(v, v_):
+    return np.sqrt(np.mean((v_ - v)**2)) / normal_std(v)
